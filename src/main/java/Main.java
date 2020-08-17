6a24dd866c64b721374208ba6fae9fed017427c4
LeeJeongSeok <ljs0429777@gmail.com>
2020-08-17 20:34:26 +0900

    add : TO-DO UI 완성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class Main {
@@ -6,19 +8,29 @@ public class Main {
         System.out.println("hello world");
 
         JFrame todoFrame = new JFrame();
-        todoFrame.setSize(500, 300);
+        todoFrame.setSize(600, 400);
         todoFrame.setDefaultCloseOperation(todoFrame.EXIT_ON_CLOSE);
+        todoFrame.setLocationRelativeTo(null);
+
+        JPanel todoPanel = new JPanel(new BorderLayout());
+
+        JTextArea todoListArea = new JTextArea();
+        todoListArea.setText("====== Today's To-Do List ======");
+        todoListArea.setBorder(new TitledBorder("todoList"));
+
+        JTextArea finishListArea = new JTextArea();
+        finishListArea.setText("====== Today's Finish List ======");
+        finishListArea.setBorder(new TitledBorder("finishList"));
+
+        JTextField inputTodo = new JTextField();
+        inputTodo.setBorder(new TitledBorder("input todo"));
+
+        todoPanel.add(todoListArea, BorderLayout.WEST);
+        todoPanel.add(finishListArea, BorderLayout.CENTER);
+        todoPanel.add(inputTodo, BorderLayout.SOUTH);
 
 
-
-
-        JPanel todoPanel = new JPanel();
-
-
-        JTextArea textArea = new JTextArea("test");
-
-
-
+        todoFrame.setContentPane(todoPanel);
         todoFrame.setVisible(true);
     }
 }

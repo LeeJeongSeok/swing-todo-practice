84e5863b03bc268985cd992d8ef3a77b531e3d4e
LeeJeongSeok <ljs0429777@gmail.com>
2020-08-20 23:06:29 +0900

    할일 추가 및 완료 기능 추가

--- src/main/java/TodoPanel.java
+++ src/main/java/TodoPanel.java
@@ -1,13 +1,19 @@
+import javafx.scene.input.KeyCode;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class TodoPanel extends JPanel implements ActionListener {
+public class TodoPanel extends JPanel implements ActionListener{
 
+    JPanel inputPanel;
     JTextArea todoListArea, finishListArea;
-    JTextField inputTodo;
+    JTextField todoInputField;
+    JButton addTodo, finishTodo;
 
     public TodoPanel() {
         initComponent();
@@ -19,28 +25,57 @@ public class TodoPanel extends JPanel implements ActionListener {
     private void initComponent() {
         todoListArea = new JTextArea();
         finishListArea = new JTextArea();
-        inputTodo = new JTextField();
+        todoInputField = new JTextField();
+        inputPanel = new JPanel(new FlowLayout());
+        inputPanel.setBorder(new TitledBorder("Input Area"));
+
+        addTodo = new JButton("add");
+        finishTodo = new JButton("finish");
+        todoInputField.setPreferredSize(new Dimension(400, 50));
+
+
+        addTodo.addActionListener(this);
+        finishTodo.addActionListener(this);
     }
 
     private void setComponentTitleAndBorder() {
-        todoListArea.setText("====== Today's To-Do List ======");
+        todoListArea.setText("====== Today's To-Do List ======\n");
         todoListArea.setBorder(new TitledBorder("todoList"));
 
-        finishListArea.setText("====== Today's Finish List ======");
+        finishListArea.setText("====== Today's Finish List ======\n");
         finishListArea.setBorder(new TitledBorder("finishList"));
-
-        inputTodo.setBorder(new TitledBorder("input todo"));
     }
 
     private void locatedComponent() {
         add(todoListArea, BorderLayout.WEST);
         add(finishListArea, BorderLayout.CENTER);
-        add(inputTodo, BorderLayout.SOUTH);
+        add(inputPanel, BorderLayout.SOUTH);
+
+        inputPanel.add(todoInputField);
+        inputPanel.add(addTodo);
+        inputPanel.add(finishTodo);
     }
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("add todo task!");
+
+        if (e.getSource().equals(addTodo)) {
+            addTask(todoInputField.getText());
+        } else {
+            finishTask(todoInputField.getText());
+        }
     }
 
+    private void addTask(String task) {
+        if (!task.equals("")) {
+            todoListArea.append(task + "\n");
+            todoInputField.setText("");
+        }
+    }
 
+    private void finishTask(String task) {
+        if (!task.equals("")) {
+            finishListArea.append(task + "\n");
+            todoInputField.setText("");
+        }
+    }
 }
